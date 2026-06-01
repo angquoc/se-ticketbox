6bafcccc9cb35f8a9de6d1bcd5a42232db4f6ed6
aster <thuyhuongac12@gmail.com>
2026-06-02 02:54:46 +0700

    chore(configs): initialize folder structure

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './database/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
