78d6c8dd111396615b82835dc0854f9c456e8925
angquoc <nguyenthianhngocnc2005@gmail.com>
2026-06-02 20:12:48 +0700

    chore(auth): remove testing endpoint and fix linting assignment error

--- src/backend/src/modules/auth/auth.controller.ts
+++ src/backend/src/modules/auth/auth.controller.ts
@@ -1,11 +1,7 @@
-import { Controller, Post, Body, Get, UseGuards, Request } from '@nestjs/common';
-import { Role } from '@prisma/client'; // Import Role
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
-import { AuthGuard } from './guards/auth.guard'; // Import AuthGuard
-import { RolesGuard } from './guards/roles.guard'; // Import RolesGuard
-import { Roles } from './decorators/roles.decorator'; // Import Roles Decorator
 
 @Controller('auth')
 export class AuthController {
@@ -19,16 +15,5 @@ export class AuthController {
   @Post('login')
   login(@Body() body: LoginDto) {
     return this.authService.login(body);
-  }
-
-  // --- API TEST GUARD ---
-  @Get('me')
-  @UseGuards(AuthGuard, RolesGuard) // Phải có token VÀ phải đúng role
-  @Roles(Role.ORGANIZER) // Chỉ cho phép Ban tổ chức
-  getProfile(@Request() req: any) {
-    return {
-      message: 'Xin chào Ban tổ chức, bạn đã lọt qua Guard thành công!',
-      user: req.user,
-    };
   }
 }
